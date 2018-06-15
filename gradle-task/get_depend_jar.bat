f7e262e2573e857b49b7e6c925736b43db34642e
rei.shibano <shibano@s-itsol.net>
2018-06-16 03:09:59 +0900

    gradleでjarライブラリ取得

---
+++ gradle-task/get_depend_jar.bat
@@ -0,0 +1,8 @@
+
+set GRADLE_HOME=C:\Dev\Java\build-tool\gradle-4.8
+set Path=%Path%;C:\Dev\Java\build-tool\gradle-4.8\bin
+
+cd C:\Dev\Java\workspace\victoria-libs\gradle-task
+
+gradle getDependJar
+
